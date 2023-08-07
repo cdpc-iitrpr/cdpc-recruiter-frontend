3518b84594fb02eb36528ea669e71f72dc8743f4
SukhmeetSingh <78161187+SukhmeetSingh2002@users.noreply.github.com>
2023-08-07 14:33:29 +0530

    add login page

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 import Header from './components/Header'
 import Landing from './pages/Landing'
+import Email from './pages/Email';
 
 function App() {
 
   return (
     <>
       <div className="App">
-        <Header />
-        <Landing />
+        {/* <Header /> */}
+        {/* <Landing /> */}
+        <Email />
       </div>
     </>
   )
